30f814e49a7be1be79d6a73abbc9d18a2a50d6eb
heesun-hong <dukye2001@naver.com>
2019-11-08 12:50:02 +0900

    fifth

--- main1.c
+++ main1.c
@@ -3,12 +3,26 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void main(void) {
-	int i = 300;
+void main(void) 
+{
+	int i;
+	int grade[5];
+	int average = 0;
+	int *gptr;
 	
-	int *pi = &i;
-	char *pc = &i;
+	for (i=0;i<5;i++)
+	{
+		printf("grade[%i] = ", i);
+		scanf("%d", &grade[i]);
+	}
+	gptr = grade;
+	for (i=0;i<5;i++)
+	{
+		printf("grade[%d] = %d\n", i, *(gptr+i));
+		average += *(gptr+i);
+	}
+	printf("\n -- average : %i\n", average/5);
 	
-	printf("%i, %i, %i\n", i, *pi, *pc);
+	return 0;
 }
 
